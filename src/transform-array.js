ba34fa1e1dc28c3854d347b90b87597c30ae9656
koeebeth <96339397+koeebeth@users.noreply.github.com>
2023-12-14 00:27:05 +0300

    completed transform array

--- src/transform-array.js
+++ src/transform-array.js
@@ -13,8 +13,53 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
  */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
+function transform(arr) {
+  if(!(arr instanceof Array)) throw new Error("'arr' parameter must be an instance of the Array!");
+
+  const transformedArray = [...arr];
+
+  const isNotAction = (el) => {
+    return (
+      el !== '--discard-next'
+    && el !== '--discard-prev'
+    && el !== '--double-next' 
+    && el !== '--double-prev');
+  }
+
+  let i = 0;
+  while (transformedArray[i]) {
+    switch (transformedArray[i]) {
+      case '--discard-next':
+        if (transformedArray[i + 1] && isNotAction(transformedArray[i + 1])){
+          transformedArray.splice(i + 1, 1);
+        }
+        i++;
+        break;
+      case '--discard-prev':
+        if (transformedArray[i - 1] && isNotAction(transformedArray[i - 1])) {
+          transformedArray.splice(i - 1, 1);
+        }
+        i++;
+        break;
+      case '--double-next':
+        if (transformedArray[i + 1] && isNotAction(transformedArray[i + 1])){
+          transformedArray.splice(i + 1, 0, transformedArray[i + 1]);
+        }
+        i++;
+        break;
+      case '--double-prev':
+        if (transformedArray[i - 1] && isNotAction(transformedArray[i - 1])) {
+          transformedArray.splice(i - 1, 0, transformedArray[i - 1]);
+        }
+        i+=2;
+        break;
+      default:
+        i++;
+        break;
+    }
+  }
+
+  return transformedArray.filter(el => isNotAction(el));
   // remove line with error and write your code here
 }
 
