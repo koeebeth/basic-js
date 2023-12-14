ba1d457f09d95377f72cc25c50c9fdc2bd45fe59
koeebeth <96339397+koeebeth@users.noreply.github.com>
2023-12-14 22:42:21 +0300

    completed cipher

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -19,14 +19,77 @@ const { NotImplementedError } = require('../extensions/index.js');
  * reverseMachine.decrypt('AEIHQX SX DLLU!', 'alphonse') => '!NWAD TA KCATTA'
  * 
  */
+const encryptLetter = (key, letter) => {
+  letter = letter.toUpperCase();
+  key = key.toUpperCase();
+
+  let charCode = 'A'.charCodeAt(0) + ((key.charCodeAt(0) - 'A'.charCodeAt(0)) + (letter.charCodeAt(0) - 'A'.charCodeAt(0))) % 26;
+  return String.fromCharCode(charCode);
+}
+
+const decryptLetter = (key, letter) => {
+  letter = letter.toUpperCase();
+  key = key.toUpperCase();
+
+  let charCode = 'A'.charCodeAt(0) + ((letter.charCodeAt(0) - 'A'.charCodeAt(0)) - (key.charCodeAt(0) - 'A'.charCodeAt(0)) + 26) % 26;
+  return String.fromCharCode(charCode);
+}
+
+
+const isAlpha = (letter) => {
+  letter = letter.toUpperCase();
+  return letter.charCodeAt(0) >= 'A'.charCodeAt(0) && letter.charCodeAt(0) <= 'Z'.charCodeAt(0);
+}
+
+const encryptFn = (str, key) => {
+  if(str === undefined || key === undefined) {
+    throw new Error('Incorrect arguments!');
+  }
+  let encrypted = '';
+  let indx = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (isAlpha(str[i])) {
+      encrypted += encryptLetter(key[indx % key.length], str[i]);
+      indx++;
+    }
+    else {
+      encrypted += str[i];
+    }
+  }
+  return encrypted;
+}
+
+const decryptFn = (str, key) => {
+  if(str === undefined || key === undefined) {
+    throw new Error('Incorrect arguments!');
+  }
+  let decrypted = '';
+  let indx = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (isAlpha(str[i])) {
+      decrypted += decryptLetter(key[indx % key.length], str[i]);
+      indx++;
+    }
+    else {
+      decrypted += str[i];
+    }
+  }
+  return decrypted;
+}
+
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new NotImplementedError('Not implemented');
+  constructor(isDirect) {
+    if (!(isDirect === true || isDirect === undefined)) {
+      this.encrypt = (str, key) => encryptFn(str, key).split('').reverse().join('');
+      this.decrypt = (str, key) => decryptFn(str, key).split('').reverse().join('');
+    }
+  }
+  encrypt(str, key) {
+    return encryptFn(str, key);
     // remove line with error and write your code here
   }
-  decrypt() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  decrypt(str, key) {
+    return decryptFn(str, key);
   }
 }
 
