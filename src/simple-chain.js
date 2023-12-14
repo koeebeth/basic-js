6a47d626a33eb814fb6de9c5bcb1297fac605bd4
koeebeth <96339397+koeebeth@users.noreply.github.com>
2023-12-14 16:05:43 +0300

    completed chain task

--- src/simple-chain.js
+++ src/simple-chain.js
@@ -5,24 +5,34 @@ const { NotImplementedError } = require('../extensions/index.js');
  * 
  */
 const chainMaker = {
+  chain: [],
   getLength() {
-    throw new NotImplementedError('Not implemented');
+    return this.chain.length;
     // remove line with error and write your code here
   },
-  addLink(/* value */) {
-    throw new NotImplementedError('Not implemented');
+  addLink(value = '') {
+    this.chain.push(`( ${value} )`);
+    return this;
     // remove line with error and write your code here
   },
-  removeLink(/* position */) {
-    throw new NotImplementedError('Not implemented');
+  removeLink(position) {
+    if (parseInt(position) !== position || +position - 1 >= this.chain.length || +position <= 0){
+      this.chain = [];
+      throw new Error("You can't remove incorrect link!");
+    }
+    this.chain.splice(position - 1, 1);
+    return this;
     // remove line with error and write your code here
   },
   reverseChain() {
-    throw new NotImplementedError('Not implemented');
+    this.chain.reverse();
+    return this;
     // remove line with error and write your code here
   },
   finishChain() {
-    throw new NotImplementedError('Not implemented');
+    const chain = [...this.chain];
+    this.chain = [];
+    return chain.join('~~');
     // remove line with error and write your code here
   }
 };
